43bdc762fea95e43a43db3062ee46c7967d90779
s2100688 <89017210+s2100688@users.noreply.github.com>
2022-04-22 01:22:43 +0300

    added backend delivery of data

--- src/backend/firebase-functions.js
+++ src/backend/firebase-functions.js
@@ -1,19 +1,38 @@
 import { db } from "../backend/firebase-config";
 import { collection, addDoc } from "firebase/firestore";
+import axios from "axios";
 
-export const firebaseUpload = async (results) => {
+// export const firebaseUpload = async (results) => {
+//   console.log("Results from firebaseUpload:");
+//   console.log(results);
+//   const isEmpty = Object.keys(results).length === 0;
+
+//   console.log("object is empty? :", isEmpty);
+//   if (!isEmpty) {
+//     const docRef = collection(db, "survey_results");
+//     try {
+//       await addDoc(docRef, results);
+//       console.log("data sent to firebase");
+//     } catch (error) {
+//       console.log("error: ", error);
+//     }
+//   }
+// };
+
+export const firebaseUpload = (results) => {
   console.log("Results from firebaseUpload:");
   console.log(results);
   const isEmpty = Object.keys(results).length === 0;
 
   console.log("object is empty? :", isEmpty);
   if (!isEmpty) {
-    const docRef = collection(db, "survey_results");
     try {
-      await addDoc(docRef, results);
-      console.log("data sent to firebase");
+      axios.post(
+        "https://us-central1-promoterscore-14480.cloudfunctions.net/api/data",
+        results
+      );
     } catch (error) {
-      console.log("error: ", error);
+      console.log("Error: ", error);
     }
   }
 };
